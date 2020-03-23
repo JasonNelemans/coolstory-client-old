2e2ca574058278d343ac361962da3cadaff9ddc8
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-23 14:08:55 +0100

    created home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+export default function Home() {
+  return (
+    <Jumbotron >
+      <h1>THIS IS THE HOMEPAGE!</h1>
+    </Jumbotron>
+  )
+}
