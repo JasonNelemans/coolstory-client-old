69546160c21deeab4b6ac54b208ec3c36c627384
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-23 14:17:46 +0100

    created simple actions thunk

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
+import { fetchHomepages } from '../../store/home/actions';
 
 export default function Home() {
+
+  fetchHomepages();
+  
   return (
     <Jumbotron >
       <h1>THIS IS THE HOMEPAGE!</h1>
---
+++ src/store/home/actions.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { apiUrl } from "../../config/constants";
+
+export function fetchHomepages() {
+    console.log('Hi from inside the actions folder.')
+}
