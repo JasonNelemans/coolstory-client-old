c0c838c0d30d9cb1ff2d994f75215e41108cbb55
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-23 15:42:46 +0100

    render homepages on homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import { fetchHomepages } from '../../store/home/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectHomepages } from '../../store/home/selector';
+import Homepage from '../../components/Homepage';
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const homepages = useSelector(selectHomepages);
 
-  fetchHomepages();
-  
+  useEffect(() => {
+    dispatch(fetchHomepages());
+  }, []);
+
+  console.log('homepages: ', homepages)
   return (
-    <Jumbotron >
-      <h1>THIS IS THE HOMEPAGE!</h1>
-    </Jumbotron>
+    <div>
+      <Jumbotron >
+        <h1>THIS IS THE HOMEPAGE!</h1>
+      </Jumbotron>
+      {homepages.map((homepage) => {
+        return (
+          <Homepage 
+            title={homepage.title}
+            description={homepage.description}
+            id={homepage.userId}
+            key={homepage.id}
+          />
+        )
+      })}
+    </div>
   )
 }
