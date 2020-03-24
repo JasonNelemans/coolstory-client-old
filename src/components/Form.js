6aa57bf193251a34871d59ce861b9775ed1c87fb
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-24 16:47:26 +0100

    created dynamic form display, started Form component

---
+++ src/components/Form.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { selectToken } from "../../store/user/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, Link } from "react-router-dom";
+import { Col } from "react-bootstrap";
+
+export default function Form() {
+  return (
+    <div className='form'>
+       <Container>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+        <h1 className="mt-5 mb-5">Signup</h1>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={name}
+            onChange={event => setName(event.target.value)}
+            type="text"
+            placeholder="Enter name"
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mt-5">
+          <Button variant="primary" type="submit" onClick={submitForm}>
+            Sign up
+          </Button>
+        </Form.Group>
+        <Link to="/login">Click here to log in</Link>
+      </Form>
+    </Container>
+    </div>
+  )
+}
--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
 import { selectUserHomepage, selectUserStories } from '../../store/user/selectors';
 import Story from '../../components/Story';
+import Button from 'react-bootstrap/Button';
 
 export default function MyPage() {
   const homepage = useSelector(selectUserHomepage);
   const stories = useSelector(selectUserStories);
-
-  console.log('homepage in mypage: ', homepage);
-  console.log('stories in mypage: ', stories)
+  const [display, setDisplay] = useState('none')
 
   if(!homepage || !stories) {
     return <h1>Loading...</h1>
   }
+  
+  const clickHandler = () => {
+    display === 'none' ? setDisplay(true) : setDisplay('none')
+  }
+
+  const form = (
+    <div style={{display: display}}>
+      <h1>Hello</h1>
+    </div>
+  )
 
   return (
     <div className='myPage'>
+      {form}
+      <Button onClick={clickHandler}>Post a cool story bro</Button>
       <div className='myHomepage' 
         style={{
           backgroundColor: homepage.backgroundColor, 
