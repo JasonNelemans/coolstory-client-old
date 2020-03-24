7ab5ab3614575eba348e08b8f52198ec08c5cf3f
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-24 12:06:22 +0100

    created user selectors, logged myhomepage/story

--- src/pages/MyPage/index.js
+++ src/pages/MyPage/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUserHomepage, selectUserStories } from '../../store/user/selectors';
+
 
 export default function MyPage() {
+  const homepage = useSelector(selectUserHomepage);
+  const stories = useSelector(selectUserStories);
+
+  console.log('homepage in mypage: ', homepage);
+  console.log('stories in mypage: ', stories)
   return (
     <div>This is my page.</div>
   )
