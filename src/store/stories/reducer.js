b54b79da1ecfd00172437d2daa8e2ad1714676e0
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-25 11:48:24 +0100

    updated reducer

--- src/store/stories/reducer.js
+++ src/store/stories/reducer.js
@@ -3,7 +3,7 @@ const initialState = [];
 export default (state = initialState, action) => {
   switch(action.type) {
     case 'FETCH_STORIES_SUCCES': 
-      return [ ...action.payload]
+      return [ ...action.payload];
     default:
       return state;
   }
